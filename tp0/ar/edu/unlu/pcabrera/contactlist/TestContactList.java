4401bf9dd79b04b322cec7ecbcdb185b47c8b11a
Pablo Cabrera <binchmod777@gmail.com>
2016-03-31 10:12:48 -0300

    Use for() loop in TestContactList instead Iter.next()

--- tp0/ar/edu/unlu/pcabrera/contactlist/TestContactList.java
+++ tp0/ar/edu/unlu/pcabrera/contactlist/TestContactList.java
@@ -4,24 +4,27 @@ import java.util.Iterator;
 
 class TestContactList {
 	public static void main (String[] args) {
+		/* Simple example for testing contact sorting and listing */
+
 		ContactList clist;
 		Contact contact;
 		Iterator iter;
 
+		/* Create contact list and add some contacts */
 		clist = new ContactList();
-
 		clist.addContact (new Contact ("Paula", "023424285"));
 		clist.addContact (new Contact ("Ignacio", "03354289"));
 		clist.addContact (new Contact ("Sofia", "3535073"));
 		clist.addContact (new Contact ("Julian", "129490"));
-		clist.addContact (new Contact ("Priscila", "0344581"));
+		clist.addContact (new Contact ("Camila", "0344581"));
 		clist.addContact (new Contact ("Mariano", "9938202"));
 		clist.addContact (new Contact ("Melina", "857492"));
 		clist.addContact (new Contact ("Jorge", "0058332859"));
 
-		iter = clist.iterator ();
-		while (iter.hasNext()) {
-			contact = (Contact) iter.next();
+		/* Show contact list */
+		/* We can use "for" because ContactList implements Iterable interface */
+		for (Object element: clist) {
+			contact = (Contact) element;
 			System.out.println ("NOMBRE: " + contact.getName() +"	TELEFONO: " + contact.getPhone());
 		}
 	}
