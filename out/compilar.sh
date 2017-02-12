d8275c0ce7b18c279cb1a3b751c41f16f0a9e90f
Pablo Cabrera <binchmod777@gmail.com>
2017-02-12 01:09:37 -0300

    compilar.sh

--- out/compilar.sh
+++ out/compilar.sh
@@ -5,8 +5,8 @@ mkdir -p out/tp1
 mkdir -p out/tp2
 mkdir -p out/misc
 
-javac -d out tp1/*/*.java
-javac -d out tp2/*/*.java
+javac -cp .:/usr/share/java/jsoup.jar -d out tp1/*/*.java
+javac -cp .:/usr/share/java/jsoup.jar -d out tp2/*/*.java
 javac -d out misc/*.java
 
 cd out
